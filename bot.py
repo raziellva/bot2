cefa61af8729fe25fceee08bfcb1df517e070932
raziellva <razielleyvaperez4338@gmail.com>
2025-09-15 06:23:39 -0400

    Add files via upload

--- bot.py
+++ bot.py
@@ -161,7 +161,7 @@ async def get_db_command(client, message):
             # Enviar el archivo
             await message.reply_document(
                 document=tmp_file.name,
-                caption="📊 Copia de la base de datos de usuarios\n👤**Usuarios:**  {len(users_data)}"
+                caption="📊 Copia de la base de datos de usuarios\n👤 {len(users_data)} Éxito"
             )
             
             # Eliminar el archivo temporal
